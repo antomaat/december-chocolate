da9d1e386649f43f3e0eeee0b5a6fa7d51e7f47e
anto <antoalasepp@gmail.com>
2023-12-11 21:24:43 +0200

    messed up day 7 part 2

--- day7/index.js
+++ day7/index.js
@@ -3,11 +3,12 @@ import fs from "fs";
 fs.readFile("input.txt", "utf8", (err, data) => {
     const input = data.split('\n').filter(x => x.length > 0).map(value => value.split(' '));
     //console.log(input);
-    solvePart1(input);
+    //solvePart1(input);
+    solvePart2(input);
     //console.log(matchType("23456"))
 });
 
-const cards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"];
+const cards = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"];
     // high card, one pair, two pair, three of a kind, full house, four of a kind, five of a kind
 const hands = [[],[],[],[],[],[],[],];
 function solvePart1(input) {
@@ -52,6 +53,21 @@ function solvePart2(input) {
             index++;
         }
     }
+    console.log("one hand");
+    console.log(hands[0]);
+    console.log("pair");
+    console.log(hands[1]);
+    console.log("two pairs");
+    console.log(hands[2]);
+    console.log("three of a kind");
+    console.log(hands[3]);
+    console.log("full house");
+    console.log(hands[4]);
+    console.log("four of a kind");
+    console.log(hands[5]);
+    console.log("five of a kind");
+    console.log(hands[6]);
+    //console.log(hands);
     console.log(sum);
     
 }
@@ -82,27 +98,26 @@ function matchType(hand) {
         handCount[index] += 1;
     }
     console.log(hand);
-    console.log(handCount);
-    if (isHighCard(hand)) {
-        return 0;
-    }
-    if (isFullHouse(handCount)) {
-        return 4;
-    }
-    if (isOnePair(handCount)) {
-        return 1;
-    }
-    if (isTwoPair(handCount)) {
-        return 2;
-    }
-    if (isThreeOfAKind(handCount)) {
-        return 3;
+    if (isFiveOfAKind(handCount)) {
+        return 6;
     }
     if (isFourOfAKind(handCount)) {
         return 5;
     }
-    if (isFiveOfAKind(handCount)) {
-        return 6;
+    if (isFullHouse(handCount)) {
+        return 4;
+    }
+    if (isThreeOfAKind(handCount)) {
+        return 3;
+    }
+    if (isTwoPair(handCount)) {
+        return 2;
+    }
+    if (isOnePair(handCount)) {
+        return 1;
+    }
+    if (isHighCard(hand)) {
+        return 0;
     }
     console.log("error happened");
     return -1;
@@ -110,12 +125,13 @@ function matchType(hand) {
 
 function isFiveOfAKind(handCount) {
     let fiveCount = 0;
+    const j = handCount[12];
     for (const count of handCount) {
-        if (count == 5) {
+        if (count + j >= 5) {
             fiveCount++;
         }
     }
-    if (fiveCount == 1) {
+    if (fiveCount >= 1) {
         return true;
     }
     return false;
@@ -123,12 +139,19 @@ function isFiveOfAKind(handCount) {
 
 function isFourOfAKind(handCount) {
     let fourCount = 0;
+    const j = handCount[12];
+    console.log("hand count: ", handCount);
+    let index = 0;
     for (const count of handCount) {
-        if (count == 4) {
+        index++;
+        if (index == 12) {
+            break;
+        }
+        if (count + j == 4) {
             fourCount++;
         }
     }
-    if (fourCount == 1) {
+    if (fourCount >= 1) {
         return true;
     }
     return false;
@@ -137,15 +160,17 @@ function isFourOfAKind(handCount) {
 function isFullHouse(handCount) {
     let pairCount = 0;
     let threeCount = 0;
-    for (const count of handCount) {
-        if (count == 3) {
+    let j = handCount[12];
+    for (let i = 0; i < 12; i++) {
+        const count = handCount[i]
+        if (count + j == 3) {
             threeCount++;
         }
-        if (count == 2) {
+        if (count + j == 2) {
             pairCount++;
         }
     }
-    if (pairCount == 1 && threeCount == 1) {
+    if (pairCount >= 1 && threeCount >= 1) {
         return true;
     }
     return false;
@@ -154,15 +179,17 @@ function isFullHouse(handCount) {
 function isThreeOfAKind(handCount) {
     let pairCount = 0;
     let threeCount = 0;
-    for (const count of handCount) {
-        if (count == 3) {
+    const j = handCount[12];
+    for (let i = 0; i < 12; i++) {
+        const count = handCount[i]
+        if (count + j == 3) {
             threeCount++;
         }
-        if (count == 2) {
+        if (count  + j == 2) {
             pairCount++;
         }
     }
-    if (pairCount == 0 && threeCount == 1) {
+    if (pairCount == 0 && threeCount >= 1) {
         return true;
     }
     return false;
@@ -170,8 +197,10 @@ function isThreeOfAKind(handCount) {
 
 function isTwoPair(handCount) {
     let pairCount = 0;
-    for (const count of handCount) {
-        if (count == 2) {
+    const j = handCount[12];
+    for (let i = 0; i < 12; i++) {
+        const count = handCount[i]
+        if (count + j == 2) {
             pairCount++;
         }
     }
@@ -183,12 +212,14 @@ function isTwoPair(handCount) {
 
 function isOnePair(handCount) {
     let pairCount = 0;
-    for (const count of handCount) {
-        if (count == 2) {
+    const j = handCount[12];
+    for (let i = 0; i < 12; i++) {
+        const count = handCount[i]
+        if (count  + j == 2) {
             pairCount++;
         }
     }
-    if (pairCount == 1) {
+    if (pairCount >= 1) {
         return true;
     }
     return false;
