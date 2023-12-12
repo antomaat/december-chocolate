a551f8a7f45834b71a31c2e067ad01af6d7df20e
anto <antoalasepp@gmail.com>
2023-12-12 22:50:12 +0200

    day9 part 2

--- day9/index.js
+++ day9/index.js
@@ -10,10 +10,19 @@ fs.readFile("input.txt", "utf8", (err, data) => {
     for (const values of input) {
         const result = recursivePredict(values);
         sum += result[result.length - 1];
+    }
+    let sumPart2 = 0;
+    for (const values of input) {
+        const result = recursivePredictPart2(values);
+        sumPart2 += result[0];
         console.log("___________");
         console.log(result);
     }
+    //const result = recursivePredictPart2([10, 13, 16, 21, 30, 45])
+    console.log("---------------");
+    //console.log(result);
     console.log(sum);
+    console.log(sumPart2);
 });
 
 function recursivePredict(input) {
@@ -29,6 +38,23 @@ function recursivePredict(input) {
         const list = recursivePredict(newArr);
         const update = input[input.length-1] + newArr[newArr.length-1];
         input.push(update);
+        return input;
+    }  
+}
+
+function recursivePredictPart2(input) {
+    //console.log(input);
+    let newArr = [];
+    for (let i = 0; i < input.length-1;) {
+        newArr.push(input[i+1] - input[i])
+        i += 1;
+    }
+    if (newArr.filter(value => value == 0).length == newArr.length) {
+        return newArr; 
+    } else {
+        const list = recursivePredictPart2(newArr);
+        const update = input[0] - newArr[0];
+        input.unshift(update);
         return input;
     }  
 }
